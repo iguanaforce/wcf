868c3adc69606a6a5fdb0435f4747c1b3caedef9
iguanaforce <iguana.force@gmail.com>
2019-10-23 09:20:29 +0700

    1st commit

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        List<CustomerDetails> GetCustomerDetails(string CustomerName , string userPassword);
+
+        //[OperationContract]
+        //List<CustomerDetails> INSERTDB(string CustomerName);
+
+        [OperationContract]
+        string InsertCustomerDetails(CustomerDetails customerInfo);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CustomerDetails
+    {
+        string estarappid = string.Empty;
+        string taskid = string.Empty;
+        string customername = string.Empty;
+        string fieldnote = string.Empty;
+        string producttype = string.Empty;
+        string branchname = string.Empty;
+        string dtmstartsend = string.Empty;
+        string dtmendsend = string.Empty;
+        string dtmapproved = string.Empty;
+        string surveyorname = string.Empty;
+
+        [DataMember]
+        public string ESTAR_APPID
+        {
+            get { return estarappid; }
+            set { estarappid = value; }
+        }
+        [DataMember]
+        public string TASK_ID
+        {
+            get { return taskid; }
+            set { taskid = value; }
+        }
+
+        [DataMember]
+        public string CUSTOMER_NAME
+        {
+            get { return customername; }
+            set { customername = value; }
+        }
+
+        [DataMember]
+        public string FIELD_NOTE
+        {
+            get { return fieldnote; }
+            set { fieldnote = value; }
+        }
+
+        [DataMember]
+        public string PRODUCT_TYPE
+        {
+            get { return producttype; }
+            set { producttype = value; }
+        }
+
+        [DataMember]
+        public string BRANCH_NAME
+        {
+            get { return branchname; }
+            set { branchname = value; }
+        }
+
+        [DataMember]
+        public string DTM_START_SEND
+        {
+            get { return dtmstartsend; }
+            set { dtmstartsend = value; }
+        }
+
+        [DataMember]
+        public string DTM_END_SEND
+        {
+            get { return dtmendsend; }
+            set { dtmendsend = value; }
+        }
+
+        [DataMember]
+        public string DTM_APPROVED
+        {
+            get { return dtmapproved; }
+            set { dtmapproved = value; }
+        }
+        [DataMember]
+        public string SURVEYOR_NAME
+        {
+            get { return surveyorname; }
+            set { surveyorname = value; }
+        }
+    }
+}
---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml"> <head runat="server"> <title></title> </head> <body> <form id="form1" runat="server"> <div> <h2 > <strong>Customer  Form</strong></h2> </div>  
+Estar_Appid  <asp:TextBox ID="txtESTAR_APPID" runat="server"></asp:TextBox> 
+<br />  
+<br />
+ID_Task  <asp:TextBox ID="txtTASK_ID" runat="server"></asp:TextBox> 
+<br />
+<br />
+Nama Customer  <asp:TextBox ID="txtCUSTOMER_NAME" runat="server"></asp:TextBox> 
+<br />  
+<br />
+Field Note  <asp:TextBox ID="txtFIELD_NOTE" runat="server"></asp:TextBox> 
+<br />
+<br />
+Tipe Produk<asp:TextBox ID="txtPRODUCT_TYPE" runat="server"></asp:TextBox> 
+<br />  
+<br />
+Cabang  <asp:TextBox ID="txtBRANCH_NAME" runat="server"></asp:TextBox> 
+<br />
+<br />
+Waktu Kirim<asp:TextBox ID="txtDTM_START_SEND" runat="server"></asp:TextBox> 
+<br />  
+<br />
+Waktu Selesai Kirim  <asp:TextBox ID="txtDTM_END_SEND" runat="server"></asp:TextBox> 
+<br />
+<br />
+Tanggal Disetujui  <asp:TextBox ID="txtDTM_APPROVED" runat="server"></asp:TextBox> 
+<br />  
+<br />
+Nama Surveyor  <asp:TextBox ID="txtSURVEYOR_NAME" runat="server"></asp:TextBox> 
+<br />
+<br />
+<asp:Button ID="btnSubmit" runat="server" Text="Submit" onclick="btnSubmit_Click" />  
+<br />
+<asp:Label ID="lblResult" runat="server"/> <br /> 
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+    <br /> 
+  </form> </body> </html>
